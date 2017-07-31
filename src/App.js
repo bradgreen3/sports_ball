28f5481199764131b2b9ac5c089fa383f9481805
localadmin <localadmin@localadmins-iMac-2.local>
2017-07-31 13:59:44 -0500

    Part 2: Complete

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ class App extends Component {
                 PORTLAND:'Trail Blazers',
                 DALLAS: 'Mavericks',
                 BOSTON: 'Celtics'
+            },
+            images: {
+                PORTLAND: "https://lh6.googleusercontent.com/-6porP9gICeE/AAAAAAAAAAI/AAAAAAAA6g0/UZjJQQnV0w4/s0-c-k-no-ns/photo.jpg",
+                DALLAS: "https://pbs.twimg.com/profile_images/790567103614943233/0tDP7UPC_400x400.jpg",
+                BOSTON: "http://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/bos.png"
+            },
+            sites: {
+                PORTLAND: "http://www.nba.com/blazers/",
+                DALLAS: "http://www.nba.com/mavericks/",
+                BOSTON: "http://www.nba.com/celtics/"
             }
         }
 
@@ -31,7 +41,7 @@ class App extends Component {
           <h1>What is your favorite city?</h1>
           <input type="text" name="city" onChange={this.handleChange}/>
         <br></br><br></br>
-         {OutputCity(this.state.city, this.state.cityList)}
+         {OutputCity(this.state.city, this.state.cityList, this.state.images, this.state.sites)}
     </div>
 
 
@@ -40,13 +50,20 @@ class App extends Component {
 }
 
 export default App;
-const OutputCity = (city,cityList) => {
+const OutputCity = (city,cityList,images,sites) => {
 
-    console.log(cityList[city.toUpperCase()])
-    if (cityList[city.toUpperCase()]){
-        return <div>Go {cityList[city.toUpperCase()]}!</div>
+    let upCity = city.toUpperCase()
+
+    console.log(cityList[upCity])
+    if (cityList[upCity]){
+        return <div>
+            <br></br>
+            <a href={sites[upCity]}>Go {cityList[upCity]}!</a>
+            <br></br>
+            <img height="100" width="100" src={images[upCity]}/>
+        </div>
     }
-    if (city === " ") {
+    if (upCity === " ") {
         return <div>Go ahead, type in a city</div>
     }
     return city + '? Never heard of it.'
